78aadab2f617e0b33cbc314722d081a85846aa7c
kajal03 <connected.kajal@gmail.com>
2019-12-21 01:06:50 +0530

    New questions added

--- Arrays/PrimeVisits.cpp
+++ Arrays/PrimeVisits.cpp
@@ -1,31 +1,31 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
-
-bool isPrime(int n){
-	int i;
-	if(n==0 || n==1)
-		return false;
-	for(i=2;i<=n/2;i++){
-		if(n%i==0)
-			break;
-	}
-	if(i>n/2)
-		return true;
-	else
-		return false;
-}
 
 int main()
 {
-	int low, high, count,t;
+	int low, high, count,t,i,j;
 	cin>>t;
 	while(t--){
 		cin>>low>>high;
 		count=0;
-		for(int i=low;i<=high;i++)
-			if(isPrime(i))
+		if(low<=2){
+			count=1;
+			low=3;
+		}
+		else if(low%2==0)
+			low++;
+		for(int j=low;j<=high;j+=2)  //not trying for even nos.
+		{
+			for(i=2;i<=sqrt(j);i++){
+				if(j%i==0)
+					break;
+			}
+			if(i>sqrt(j))
 				count++;
+			
+		}
 					
-		cout<<count;
+		cout<<count<<endl;
 	}
 }
