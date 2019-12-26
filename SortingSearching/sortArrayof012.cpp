1dde4fe219be284d499d02abf66dd252774d2584
kajal03 <connected.kajal@gmail.com>
2019-12-26 16:49:13 +0530

    New questions added

--- SortingSearching/sortArrayof012.cpp
+++ SortingSearching/sortArrayof012.cpp
@@ -1,31 +1,28 @@
+//Given an array of 0's, 1's amd 2's, sort the array efficiently
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int a[]={0,0,1,1,1,2,2,2,2};
-	int n=sizeof(a)/sizeof(int), start=0,end=n-1,i;
+	int a[100], n,i;
+	cin>>n;
+	int start=0,end=n-1;
+
+	for(i=0;i<n;i++)
+		cin>>a[i];
 
 	i=0;
-	while(a[i]==0)
-	{
-		start++;
-	}
-	i=end;
-	while(a[i]==2){
-		end--;
-	}
-	i=start+1;
 	while(i<=end){
-		if(a[i]==0){
+		if(a[i]==0){ 
 			swap(a[start],a[i]);
-			start++;
+			start++; i++;
 
 		}
-		if(a[i]==2){
+		else if(a[i]==2){
 			swap(a[i],a[end]);
 			end--;
 		}
-		if(a[i]==1)
+		else if(a[i]==1)
 			i++;
 	}
 	for(i=0;i<n;i++)
